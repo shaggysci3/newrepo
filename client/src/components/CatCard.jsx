79a474598c175ac2ff5d0f42f4ade2540f235979
matthew <shaggysci3@gmail.com>
2024-08-12 11:22:52 -0400

    adding bottom text

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -2,13 +2,13 @@ const CatCard = ({ img,name }) => {
   
 
   return (
-    <>
+    <div style={{display:"flex",flexDirection:"column",fontFamily:"'Playmaker', sans-serif"}}>
       <div className="card">
           <h3 className="clothing">{name}</h3>
           <img style={{}} className="cardImg" src={img} alt="Mini" />
         </div>
-        {/* <h1>Name</h1> */}
-    </>
+        <h1>{name}</h1>
+    </div>
   );
 };
 
