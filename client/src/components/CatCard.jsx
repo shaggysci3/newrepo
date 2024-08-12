f03abeb76baba8132927692bec856a0c4bbbab9c
matthew <shaggysci3@gmail.com>
2024-08-12 12:49:44 -0400

    adding supabase imgs

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -1,11 +1,11 @@
 const CatCard = ({ img,name }) => {
-  
+  const CDNURL = "https://hzlezhtofxpfmuntarie.supabase.co/storage/v1/object/sign/shop/"
 
   return (
     <div style={{display:"flex",flexDirection:"column",fontFamily:"'Playmaker', sans-serif"}}>
       <div className="card">
           <h3 className="clothing">{name}</h3>
-          <img style={{}} className="cardImg" src={img} alt="Mini" />
+          <img style={{}} className="cardImg" src={CDNURL+img} alt="Mini" />
         </div>
         <h1>{name}</h1>
     </div>
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,7 +2,7 @@ import CatCard from "../components/CatCard"
 
 
 const Home = () =>{
- 
+ const CDNURL = "https://hzlezhtofxpfmuntarie.supabase.co/storage/v1/object/sign/shop/"
 
  
    
@@ -12,13 +12,14 @@ const Home = () =>{
       <div className='homePage'>
           {/* <img src='https://imgs.search.brave.com/oaR-MXzIsAcvQZalc-WeesYVq7ENiBmx9cAvsNiLV-o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA3LzQ4LzI4LzY5/LzM2MF9GXzc0ODI4/Njk5OF9xMlNDcllW/d3preEl5c1B4bUlk/TklKQ1BmMlJtS21C/dy5qcGc' className='bannerImg'></img> */}
           <div className='cardContainer'>
-            <CatCard name={"Cabs"} img={'./src/assets/Cabochon.png'}/>
+            <img src=""></img>
+            <CatCard name={"Cabs"} img={'Screenshot%202024-08-06%20000101.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzaG9wL1NjcmVlbnNob3QgMjAyNC0wOC0wNiAwMDAxMDEucG5nIiwiaWF0IjoxNzIzNDgxMjkzLCJleHAiOjE3NTUwMTcyOTN9.xiyz7tFUGWstHfxVdhFSkGds4el1SSW4b1Gg-93Wq6o&t=2024-08-12T16%3A48%3A13.972Z'}/>
             
-            <CatCard name={"Gems"} img={'https://imgs.search.brave.com/HEdeqpfnZMmLSjFuyqTr4wqiAOhPUaPTOMoSnURCQP4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTMy/MjY0MjUxNC92ZWN0/b3IvZ2VtLXZlY3Rv/ci1pY29ucy5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9ZGZI/NEl2MkhCWE51SWF1/VXJ2cmxvb2dsc3Fa/aFQxQ1hoYU1YRWJT/d2pYbz0'}/>
+            <CatCard name={"Gems"} img={''}/>
             
-            <CatCard name={"Resin Planar"} img={'./src/assets/Resin.png'}/>
+            <CatCard name={"Resin Planar"} img={'Screenshot%202024-08-05%20223720.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzaG9wL1NjcmVlbnNob3QgMjAyNC0wOC0wNSAyMjM3MjAucG5nIiwiaWF0IjoxNzIzNDgxMjU0LCJleHAiOjE3NTUwMTcyNTR9.cELz21JtxCg8FY2x2rdoBKXD3Uf_1C_aFyLt7Iu5iE4&t=2024-08-12T16%3A47%3A34.408Z'}/>
             
-            <CatCard name={"Mini Planar"} img={'./src/assets/Mini.png'}/>
+            <CatCard name={"Mini Planar"} img={'Screenshot%202024-08-05%20223705.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzaG9wL1NjcmVlbnNob3QgMjAyNC0wOC0wNSAyMjM3MDUucG5nIiwiaWF0IjoxNzIzNDgwNjQ0LCJleHAiOjE3NTUwMTY2NDR9.5zYMyaqO6H3aOpye7ZETLgOhpr1mP8t4_aGCh5Qwrqc&t=2024-08-12T16%3A37%3A24.620Z'}/>
           </div>
         </div>
       </>
